222fa99e2c95ea720aa43952ff16e6b4f8bc6e37
javidpoornasir <javidp.sf@gmail.com>
2019-04-13 06:04:51 -0700

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')  
+
+# using the create method to create a new product called niceCat
+niceCat = Product.create(title: "Quality Kitty",
+subtitle: "Learn App Code", image_name: "kitty.JPG", 
+price: "4.99", sku: "kittyOne", download_url: "https://cdn.macrumors.com/article-new/2018/10/appleinvitedesign3-800x509.jpg",
+details: "A really nice picture", description: %{<p>It's a cat</p>
+})
